273cf02e9d69e4e864af251d95edc46d94d85c59
Eric Davin <davinec@mail.uc.edu>
2021-04-13 13:08:44 -0400

    Initial commit

---
+++ app/src/main/java/edu/uc/group/ma5directions/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.uc.group.ma5directions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uc.group.ma5directions.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/edu/uc/group/ma5directions/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package edu.uc.group.ma5directions.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
